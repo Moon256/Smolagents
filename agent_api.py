bc32746134a29fbd6e38139168da0fe9beb5a577
Moon256 <64242706+Moon256@users.noreply.github.com>
2025-05-05 22:16:56 +0700

    Update agent_api.py

--- agent_api.py
+++ agent_api.py
@@ -1,17 +1,27 @@
 from flask import Flask, request, jsonify
 from smolagents import CodeAgent, TransformersModel, FinalAnswerTool
-from my_custom_tools import fetch_latest_news_titles_and_urls, summarize_news, extract_news_article_content, classify_topic
+from tools import (
+    fetch_latest_news_titles_and_urls,
+    extract_news_article_content,
+    summarize_news,
+    classify_topic
+)
 
 app = Flask(__name__)
 
-model = TransformersModel(model_id="Qwen/Qwen2.5-Coder-3B-Instruct")
+model = TransformersModel(
+    model_id="Qwen/Qwen2.5-Coder-3B-Instruct",
+    torch_dtype="bfloat16",
+    trust_remote_code=True,
+    max_new_tokens=2000
+)
 
 agent = CodeAgent(
     model=model,
     tools=[
         fetch_latest_news_titles_and_urls,
-        summarize_news,
         extract_news_article_content,
+        summarize_news,
         classify_topic,
         FinalAnswerTool()
     ],
@@ -28,4 +38,3 @@ def run_agent():
 
 if __name__ == "__main__":
     app.run(port=8000)
-
